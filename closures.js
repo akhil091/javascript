c5466ae8f778573c549cc78761ecff9d4c925d1e
Akhil Chandail <akhilchandail9@gmail.com>
2024-04-26 00:17:53 +0530

    Update closures.js

--- closures.js
+++ closures.js
@@ -1,74 +1,75 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Closure aur chai</title>
-</head>
-<body style="background-color: #313131;">
-    <button id="orange">Orange</button>
-    <button id="green">Green</button>
-</body>
+//Where and How to use closure concept ?
+//Private variables and functions:
+const makeCounter = () => {
+  let count = 0;
+  
+  return () => {
+    count++;
+    console.log(count);
+  }
+}
 
-    <script>
-    // function init() {
-    //     let name = "Mozilla"; 
-    //     function displayName() {
-    //         console.log(name); 
-    //     }
-    //     displayName();
-    // }
-    // init();
+let counter = makeCounter();
+counter(); // logs 1
+counter(); // logs 2
+counter(); // logs 3
 
-    // function outer(){
-    //     let username = "hitesh"
-    //     console.log("OUTER", secret);
-    //     function inner(){
-    //         let secret = "my123"
-    //         console.log("inner", username);
-    //     }
-    //     function innerTwo(){
-    //         console.log("innerTwo", username);
-    //         console.log(secret);
-    //     }
-    //     inner()
-    //     innerTwo()
+function add(x) {
+  return function(y) {
+    return x + y;
+  }
+}
 
-    // }
-    // outer()
-    // console.log("TOO OUTER", username);
+let add5 = add(5);
+console.log(add5(3)); // 8
 
 
-    // function makeFunc() {
-    //     const name = "Mozilla";
-    //     function displayName() {
-    //         console.log(name);
-    //     }
-    //     return displayName;
-    // }
 
-    // const myFunc = makeFunc();
-    // myFunc();
+var createCounter = function(init) {
+  let count = init || 0; // initialize count to 0 or given value
 
-    </script>
-    <script>
-        // document.getElementById("orange").onclick = function(){
-        //     document.body.style.backgroundColor = `orange`
-        // }
-        // document.getElementById("green").onclick = function(){
-        //     document.body.style.backgroundColor = `green`
-        // }
+  const increment = function() {
+    count++;
+    return count;
+  };
 
-        function clickHandler(color){
-            // document.body.style.backgroundColor = `${color}`
+  const decrement = function() {
+    count--;
+    return count;
+  };
 
-            return function(){
-                document.body.style.backgroundColor = `${color}`
-            }
+  const reset = function() {
+    count = init || 0;
+    return count;
+  };
+
+  return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+
+
+
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
         }
+    }
+};
 
-        document.getElementById('orange').onclick = clickHandler("orange")
-        document.getElementById('green').onclick = clickHandler("green")
+/* For example, when expect(5).toBe(4) is called,
+   val is the expect parameter (so val equals 5),
+   val2 is the toBe parameter (so val2 equals 4).
+   Since val !== val2, aka 5 != 4, we throw error "Not Equal". */
 
-    </script>
-</html>
